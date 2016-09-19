9b2429fa28e808e80c8372c179bd03e1709d7ff2
Britt Treece <btreece@bytekinetic.com>
2016-09-19 18:11:01 -0500

    update the metadata version

--- cookbooks/sensu_server/metadata.rb
+++ cookbooks/sensu_server/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'btreece@bytekinetic.com'
 license 'all_rights'
 description 'Installs/Configures sensu_server'
 long_description 'Installs/Configures sensu_server'
-version '0.1.3'
+version '0.1.4'
 
 depends 'sensu'
 depends 'uchiwa'
