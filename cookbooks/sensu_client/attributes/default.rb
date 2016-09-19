43ef6d430a30d60207ad75757c3d9205abbf0742
Britt Treece <btreece@bytekinetic.com>
2016-09-19 18:13:30 -0500

    use latest sensu version

--- cookbooks/sensu_client/attributes/default.rb
+++ cookbooks/sensu_client/attributes/default.rb
@@ -1,7 +1,8 @@
-default["sensu"]["use_embedded_ruby"] = true
+default['sensu']['use_embedded_ruby'] = true
+default['sensu']['version'] = '0.26.1-1'
+
 default[:sensu_client][:additional_attributes] = Mash.new
 default[:sensu_client][:sensu_plugin_version] = false
-
 default[:sensu_client][:subscriptions] = ['all'] + node[:roles]
 
 default[:sensu][:rabbitmq][:host] = '127.0.0.1'
