1fa2e15d151312caaa7f454e57a965a2ba24d4a7
Britt Treece <btreece@bytekinetic.com>
2016-09-27 22:04:50 -0500

    add the gelf plugin from file until the github version is fixed

--- cookbooks/log_agent/recipes/plugins.rb
+++ cookbooks/log_agent/recipes/plugins.rb
@@ -2,7 +2,14 @@ td_agent_gem 'gelf' do
   plugin false
 end
 
-td_agent_plugin 'out_gelf' do
-  url 'https://raw.githubusercontent.com/emsearcy/fluent-plugin-gelf/master/lib/fluent/plugin/out_gelf.rb'
-end
+#td_agent_plugin 'out_gelf' do
+#  url 'https://raw.githubusercontent.com/emsearcy/fluent-plugin-gelf/master/lib/fluent/plugin/out_gelf.rb'
+#end
 
+cookbook_file '/etc/td-agent/plugin/out_gelf.rb' do
+  source 'plugins/out_gelf.rb'
+  owner 'td-agent'
+  group 'td-agent'
+  mode 0755
+  action :create
+end
